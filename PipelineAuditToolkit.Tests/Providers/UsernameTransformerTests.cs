840308ce70619f62be0a2292e8becca918496d65
Dan Piessens <dan.piessens@gmail.com>
2016-08-12 14:10:48 -0500

    Added username regex and fixed username transformations.

--- PipelineAuditToolkit.Tests/Providers/UsernameTransformerTests.cs
+++ PipelineAuditToolkit.Tests/Providers/UsernameTransformerTests.cs
@@ -8,13 +8,17 @@ namespace PipelineAuditToolkit.Tests
     [TestClass]
     public class UsernameTransformerTests
     {
-        private const string AppSetting = "UserNameTransformer.Translations";
+        private const string DomainAppSetting = "UserNameTransformer.Translations";
+        private const string UserRegexAppSetting = "UserNameTransformer.RegexUserTranslations";
+        private const string UserFixedAppSetting = "UserNameTransformer.FixedUserTranslations";
 
         [TestMethod]
         public void Initialize_WhenNoDataExists_CompletesWithoutError()
         {
             var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
-            appSettings.Setup(a => a.GetApplicationSetting(AppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns((string)null);
 
             var logger = new Mock<ILogger>();
 
@@ -22,7 +26,7 @@ namespace PipelineAuditToolkit.Tests
 
             transformer.Initalize();
 
-            Assert.AreEqual(0, transformer.RuleCount);
+            Assert.AreEqual(0, transformer.DomainRuleCount);
             
             appSettings.VerifyAll();
         }
@@ -31,7 +35,10 @@ namespace PipelineAuditToolkit.Tests
         public void Initialize_WhenInvalidDataExists_CompletesWithoutErrorButIgnoresRule()
         {
             var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
-            appSettings.Setup(a => a.GetApplicationSetting(AppSetting, null)).Returns("mycompany\\.com");
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns("mycompany\\.com");
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns((string)null);
+
 
             var logger = new Mock<ILogger>();
 
@@ -39,7 +46,7 @@ namespace PipelineAuditToolkit.Tests
 
             transformer.Initalize();
 
-            Assert.AreEqual(0, transformer.RuleCount);
+            Assert.AreEqual(0, transformer.DomainRuleCount);
 
             appSettings.VerifyAll();
         }
@@ -48,7 +55,10 @@ namespace PipelineAuditToolkit.Tests
         public void Initialize_WhenValidDataExists_CompletesWithoutError()
         {
             var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
-            appSettings.Setup(a => a.GetApplicationSetting(AppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns((string)null);
+
 
             var logger = new Mock<ILogger>();
 
@@ -56,7 +66,46 @@ namespace PipelineAuditToolkit.Tests
 
             transformer.Initalize();
 
-            Assert.AreEqual(1, transformer.RuleCount);
+            Assert.AreEqual(1, transformer.DomainRuleCount);
+
+            appSettings.VerifyAll();
+        }
+
+        [TestMethod]
+        public void Initialize_WhenValidUserRegexExists_CompletesWithoutError()
+        {
+            var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns("([A-Za-z]).+\\.([A-Za-z]+):$1$0");
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns((string)null);
+
+
+            var logger = new Mock<ILogger>();
+
+            var transformer = new UsernameTransformer(appSettings.Object, logger.Object);
+
+            transformer.Initalize();
+
+            Assert.AreEqual(1, transformer.UserRegexRuleCount);
+
+            appSettings.VerifyAll();
+        }
+
+        public void Initialize_WhenValidUserFixedExists_CompletesWithoutError()
+        {
+            var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns("johndoe:jdoe");
+
+
+            var logger = new Mock<ILogger>();
+
+            var transformer = new UsernameTransformer(appSettings.Object, logger.Object);
+
+            transformer.Initalize();
+
+            Assert.AreEqual(1, transformer.UserFixedRuleCount);
 
             appSettings.VerifyAll();
         }
@@ -65,7 +114,10 @@ namespace PipelineAuditToolkit.Tests
         public void Initialize_WhenRegexIsMalformed_CompletesWithoutErrorButIgnoresRule()
         {
             var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
-            appSettings.Setup(a => a.GetApplicationSetting(AppSetting, null)).Returns("mycompany[\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns("mycompany[\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns((string)null);
+
 
             var logger = new Mock<ILogger>();
 
@@ -73,7 +125,7 @@ namespace PipelineAuditToolkit.Tests
 
             transformer.Initalize();
 
-            Assert.AreEqual(0, transformer.RuleCount);
+            Assert.AreEqual(0, transformer.DomainRuleCount);
 
             appSettings.VerifyAll();
         }
@@ -82,7 +134,10 @@ namespace PipelineAuditToolkit.Tests
         public void GetEmailAddress_WhenOriginalStringIsNotAnEmail_ReturnsValueInLowerCase()
         {
             var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
-            appSettings.Setup(a => a.GetApplicationSetting(AppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns((string)null);
+
 
             var logger = new Mock<ILogger>();
 
@@ -101,7 +156,10 @@ namespace PipelineAuditToolkit.Tests
         public void GetEmailAddress_WhenOriginalStringIsMalformedEmail_ReturnsValueInLowerCase()
         {
             var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
-            appSettings.Setup(a => a.GetApplicationSetting(AppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns((string)null);
+
 
             var logger = new Mock<ILogger>();
 
@@ -120,7 +178,10 @@ namespace PipelineAuditToolkit.Tests
         public void GetEmailAddress_WhenTransformExists_ReturnsTranslationInLowerCase()
         {
             var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
-            appSettings.Setup(a => a.GetApplicationSetting(AppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns((string)null);
+
 
             var logger = new Mock<ILogger>();
 
@@ -136,10 +197,78 @@ namespace PipelineAuditToolkit.Tests
         }
 
         [TestMethod]
+        public void GetEmailAddress_WhenUserAndDomainTransformsExists_ReturnsTranslationInLowerCase()
+        {
+            var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns(@"([A-Za-z]).+\.([A-Za-z]+):$1$2");
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns((string)null);
+
+
+            var logger = new Mock<ILogger>();
+
+            var transformer = new UsernameTransformer(appSettings.Object, logger.Object);
+
+            transformer.Initalize();
+
+            var result = transformer.GetEmailAddress("bob.vila@MYCOMPANY.com");
+
+            Assert.AreEqual("bvila@othercompany.com", result);
+
+            appSettings.VerifyAll();
+        }
+
+        [TestMethod]
+        public void GetEmailAddress_WhenUserDomainAndFixedTransformsExists_ReturnsTranslationInLowerCase()
+        {
+            var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns(@"([A-Za-z]).+\.([A-Za-z]+):$1$2");
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns("bvila:bavila");
+
+
+            var logger = new Mock<ILogger>();
+
+            var transformer = new UsernameTransformer(appSettings.Object, logger.Object);
+
+            transformer.Initalize();
+
+            var result = transformer.GetEmailAddress("bvila@MYCOMPANY.com");
+
+            Assert.AreEqual("bavila@othercompany.com", result);
+
+            appSettings.VerifyAll();
+        }
+
+        [TestMethod]
+        public void GetEmailAddress_WhenDomainAndFixedTransformsExists_ReturnsTranslationInLowerCase()
+        {
+            var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns("bvila:bavila");
+
+            var logger = new Mock<ILogger>();
+
+            var transformer = new UsernameTransformer(appSettings.Object, logger.Object);
+
+            transformer.Initalize();
+
+            var result = transformer.GetEmailAddress("bvila@MYCOMPANY.com");
+
+            Assert.AreEqual("bavila@othercompany.com", result);
+
+            appSettings.VerifyAll();
+        }
+
+        [TestMethod]
         public void GetEmailAddress_WhenTransformDoesNotExist_ReturnsValueInLowerCase()
         {
             var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
-            appSettings.Setup(a => a.GetApplicationSetting(AppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns("mycompany\\.com:othercompany.com");
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns((string)null);
+
 
             var logger = new Mock<ILogger>();
 
@@ -158,7 +287,10 @@ namespace PipelineAuditToolkit.Tests
         public void GetEmailAddress_WhenMultipleTransformsExist_ReturnsTranslationInLowerCase()
         {
             var appSettings = new Mock<IConfigurationSettings>(MockBehavior.Strict);
-            appSettings.Setup(a => a.GetApplicationSetting(AppSetting, null)).Returns("mycompany\\.com:othercompany.com;cool\\.com:othercompany2.com");
+            appSettings.Setup(a => a.GetApplicationSetting(DomainAppSetting, null)).Returns("mycompany\\.com:othercompany.com;cool\\.com:othercompany2.com");
+            appSettings.Setup(a => a.GetApplicationSetting(UserRegexAppSetting, null)).Returns((string)null);
+            appSettings.Setup(a => a.GetApplicationSetting(UserFixedAppSetting, null)).Returns((string)null);
+
 
             var logger = new Mock<ILogger>();
 
--- PipelineAuditToolkit/Providers/UsernameTransformer.cs
+++ PipelineAuditToolkit/Providers/UsernameTransformer.cs
@@ -7,30 +7,109 @@ namespace PipelineAuditToolkit.Providers
 {
     public class UsernameTransformer : IUsernameTransformer
     {
-        private const string AppSetting = "UserNameTransformer.Translations";
+        private const string DomainAppSetting = "UserNameTransformer.Translations";
+        private const string UserRegexAppSetting = "UserNameTransformer.RegexUserTranslations";
+        private const string UserFixedAppSetting = "UserNameTransformer.FixedUserTranslations";
 
         private readonly IConfigurationSettings _configurationSettings;
         private readonly ILogger _logger;
-        private readonly List<Tuple<Regex, string>> _transformations;
+        private readonly List<Tuple<Regex, string>> _domainTransformations;
+        private readonly List<Tuple<Regex, string>> _userRegexTransformations;
+        private readonly List<Tuple<string, string>> _userFixedTransformations;
 
         public UsernameTransformer(IConfigurationSettings configurationSettings, ILogger logger)
         {
             _configurationSettings = configurationSettings;
             _logger = logger;
-            _transformations = new List<Tuple<Regex, string>>();
+
+            _domainTransformations = new List<Tuple<Regex, string>>();
+            _userRegexTransformations = new List<Tuple<Regex, string>>();
+            _userFixedTransformations = new List<Tuple<string, string>>();
         }
 
-        public int RuleCount
+        public int DomainRuleCount
         {
             get
             {
-                return _transformations.Count;
+                return _domainTransformations.Count;
+            }
+        }
+
+        public int UserRegexRuleCount
+        {
+            get
+            {
+                return _userRegexTransformations.Count;
+            }
+        }
+
+        public int UserFixedRuleCount
+        {
+            get
+            {
+                return _userFixedTransformations.Count;
             }
         }
 
         public void Initalize()
         {
-            var settingData = _configurationSettings.GetApplicationSetting(AppSetting);
+            var regexFunc = new Func<string, Regex>(d => new Regex(d, RegexOptions.Compiled | RegexOptions.IgnoreCase));
+            ParseList(_domainTransformations, DomainAppSetting, "domain", regexFunc);
+
+            ParseList(_userRegexTransformations, UserRegexAppSetting, "user regex", regexFunc);
+
+            ParseList(_userFixedTransformations, UserFixedAppSetting, "user fixed", s => s);
+        }
+
+        public string GetEmailAddress(string originalEmail)
+        {
+            var domainIndex = originalEmail.IndexOf('@');
+
+            if (domainIndex > -1 && originalEmail.Length > (domainIndex + 1))
+            {
+                var userName = originalEmail.Substring(0, domainIndex);
+                var domain = originalEmail.Substring(domainIndex + 1);
+
+                var transformedDomain = MatchRegex(_domainTransformations, domain);
+
+                var transformedUser = MatchRegex(_userRegexTransformations, userName);
+                if (transformedUser == userName)
+                {
+                    foreach (var item in _userFixedTransformations)
+                    {
+                        if (string.Equals(userName, item.Item1, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            transformedUser = item.Item2;
+                            break;
+                        }
+                    }
+                }
+
+                var transformedEmail = $"{transformedUser}@{transformedDomain}";
+                _logger.WriteDebug($"Transformed user '{originalEmail}' to '{transformedEmail}'");
+                return transformedEmail;
+            }
+
+            return originalEmail.ToLower();
+        }
+
+        private static string MatchRegex(List<Tuple<Regex, string>> collection, string originalData)
+        {
+            foreach (var tranform in collection)
+            {
+                var match = tranform.Item1.Match(originalData);
+                if (match.Success)
+                {
+                    return match.Result(tranform.Item2).ToLower();
+                }
+            }
+
+            return originalData;
+        }
+
+        private void ParseList<T>(List<Tuple<T, string>> collection, string settingName, string parseType, Func<string, T> creator)
+        {
+            var settingData = _configurationSettings.GetApplicationSetting(settingName);
             if (string.IsNullOrWhiteSpace(settingData))
             {
                 return;
@@ -42,45 +121,20 @@ namespace PipelineAuditToolkit.Providers
                 var parts = item.Split(new[] { ':' }, 2);
                 if (parts.Length < 2)
                 {
-                    _logger.WriteError($"Could not parse '{item}' as a user transformation, no replace exists.");
+                    _logger.WriteError($"Could not parse '{item}' as a {parseType} transformation, no replace exists.");
                     continue;
                 }
 
                 try
                 {
-                    var regex = new Regex(parts[0], RegexOptions.Compiled | RegexOptions.IgnoreCase);
-                    _transformations.Add(new Tuple<Regex, string>(regex, parts[1]));
+                    T findItem = creator(parts[0]);
+                    collection.Add(new Tuple<T, string>(findItem, parts[1]));
                 }
                 catch (ArgumentException ex)
                 {
-                    _logger.WriteError($"Could not parse '{item}' as a user transformation. Error: {ex.Message}");
+                    _logger.WriteError($"Could not parse '{item}' as a {parseType} transformation. Error: {ex.Message}");
                 }
             }
-        }
-
-        public string GetEmailAddress(string originalEmail)
-        {
-            var domainIndex = originalEmail.IndexOf('@');
-
-            if (domainIndex > -1 && originalEmail.Length > domainIndex)
-            {
-                var userName = originalEmail.Substring(0, domainIndex);
-                var domain = originalEmail.Substring(domainIndex + 1);
-
-                foreach (var tranform in _transformations)
-                {
-                    var match = tranform.Item1.Match(domain);
-                    if (match.Success)
-                    {
-                        var transformedDomain = match.Result(tranform.Item2).ToLower();
-                        var transformedEmail = $"{userName}@{transformedDomain}";
-                        _logger.WriteDebug($"Transformed user '{originalEmail}' to '{transformedEmail}'");
-                        return transformedEmail;
-                    }
-                }
-            }
-
-            return originalEmail.ToLower();
         }
     }
 }
